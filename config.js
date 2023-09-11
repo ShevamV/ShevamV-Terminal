b110e2e26b38f10470fc98b19ac30341c7d01b24
ShevamV <136719386+ShevamV@users.noreply.github.com>
2023-09-11 22:08:15 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,17 @@
 const config = {
   'hello':'hi', 
   'hey':'namaste',
-  'help':'type \n hello, hey, lorem(), story -tc (thirsty crow)',
-  'lorem()':'lorem ipsum ',
-  'story -tc': 'Cannot fetch story. try again letter',
+  'help':'type \n hello, hey, lorem(), story -tc (thirsty crow) timetable -day(only 3 letters)',
+    'print()': 'Hello world',
+    'lorem()': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.\ Fusce venenatis, velit non scelerisque bibendum, ante quam mattis libero, a sagittis purus felis sit amet quam. Maecenas consectetur orci ac quam semper, id congue libero feugiat. Sed posuere velit ac eros ultrices scelerisque. Suspendisse fringilla, libero sit amet ultrices laoreet, quam tortor ullamcorper libero, at ullamcorper lectus odio eu tellus. In hac habitasse platea dictumst. Quisque egestas pharetra purus, id viverra tortor. Sed euismod massa a ante cursus varius. Sed ac velit vel velit porttitor dapibus. Vivamus eget eleifend risus. Vestibulum et nisi nec turpis blandit bibendum. Sed non lacinia orci. Cras tristique, nulla at ultrices sollicitudin, felis ligula tincidunt urna, at suscipit elit ex auctor libero. Suspendisse potenti.',
+    'longText()': 'This is a very long text string that spans multiple lines. It can be used to test how your terminal handles large amounts of text.',
+    'story -tc': 'Once upon a time, in a hot summer day, a thirsty crow searched for water everywhere. At last, it found a pitcher with a little water at the bottom. The crow couldn\'t reach the water, so it collected pebbles and dropped them one by one into the pitcher until the water level rose, and the crow could quench its thirst.',
+        'timetable -mon': '[SST, Science, Hindi, English || Maths, Library, Yoga, Computer]',
+        'timetable -tue': '[SST, Science, Hindi, English || Maths, Arts, Yoga, Computer]',
+        'timetable -wed': '[SST, Science, Hindi, English || Maths, Yoga, Yoga, Computer]',
+        'timetable -thu': '[SST, Science, Hindi, English || Maths, Games, Sanskrit, WE]',
+        'timetable -fri': '[SST, Science, Hindi, English || Maths, Games, Sanskrit, WE]',
+        'timetable -sat': '[SST, Science, Hindi, English || Maths, Arts, CCA, CCA]',
 };
 
 function toggleJsonInput() {
