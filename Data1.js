4b48c1b6655a17d984efbd392d4904c126ada34c
ShevamV <136719386+ShevamV@users.noreply.github.com>
2023-09-11 15:19:50 +0530

    Create Data1.js

---
+++ Data1.js
@@ -0,0 +1,20 @@
+//const config = {
+    'print()': 'Hello world',
+    'lorem()': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce venenatis, velit non scelerisque bibendum, ante quam mattis libero, a sagittis purus felis sit amet quam. Maecenas consectetur orci ac quam semper, id congue libero feugiat. Sed posuere velit ac eros ultrices scelerisque. Suspendisse fringilla, libero sit amet ultrices laoreet, quam tortor ullamcorper libero, at ullamcorper lectus odio eu tellus. In hac habitasse platea dictumst. Quisque egestas pharetra purus, id viverra tortor. Sed euismod massa a ante cursus varius. Sed ac velit vel velit porttitor dapibus. Vivamus eget eleifend risus. Vestibulum et nisi nec turpis blandit bibendum. Sed non lacinia orci. Cras tristique, nulla at ultrices sollicitudin, felis ligula tincidunt urna, at suscipit elit ex auctor libero. Suspendisse potenti.',
+    'longText()': 'This is a very long text string that spans multiple lines. It can be used to test how your terminal handles large amounts of text.',
+    'story --thirsty crow': 'Once upon a time, in a hot summer day, a thirsty crow searched for water everywhere. At last, it found a pitcher with a little water at the bottom. The crow couldn\'t reach the water, so it collected pebbles and dropped them one by one into the pitcher until the water level rose, and the crow could quench its thirst.',
+    'story --any story': 'Here is any story you want to provide as the result of the "story --any story" command.',
+    // Add more commands and their corresponding results here
+    'command1': 'Result for command1',
+    'command2': 'Result for command2',
+    'command3': 'Result for command3',
+    'command4': 'Result for command4',
+    'command5': 'Result for command5',
+    'command6': 'Result for command6',
+    'command7': 'Result for command7',
+    'command8': 'Result for command8',
+    'command9': 'Result for command9',
+    'command10': 'Result for command10'
+//};
+// remove the // from the below code if You want.
+//export default config;
